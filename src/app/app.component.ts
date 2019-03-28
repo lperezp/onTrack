6bdfb711684c7b215e6b135ba4473873afff5db0
Luis Eduardo Perez Pacherrez <luis.perez@cdiproject.com>
2019-03-28 14:47:36 -0500

    feat(selectedCliente): Se terminó la maquetación.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { ListPage } from "../pages/list/list";
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = SelectedClientePage;
 
   pages: Array<{ title: string; component: any }>;
 
