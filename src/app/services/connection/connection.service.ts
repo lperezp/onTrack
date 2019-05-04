374d4386447cca9cb9a163b2b7f8ee36a8e9f0ac
Luis Eduardo Perez Pacherrez <luis.perez@cdiproject.com>
2019-05-04 13:35:07 -0500

    feat(ConnectionService): Connection service added.

---
+++ src/app/services/connection/connection.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { HTTP } from "@ionic-native/http/ngx";
+
+const rutaAPI = "http://ontrack-dev.gerenpop.com:4000/mobile/api/v1/";
+const header = "{ 'Content-Type': 'application/json' }";
+
+@Injectable({
+  providedIn: "root"
+})
+export class ConnectionService {
+  constructor(private http: HTTP) {}
+
+  get(ruta) {
+    return this.http.get(rutaAPI + ruta, {}, {});
+  }
+
+  post(ruta, body) {
+    return this.http.post(rutaAPI + ruta, body, header);
+  }
+}
