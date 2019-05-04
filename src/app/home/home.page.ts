1560ab844403392ac4369a2258d4df4db319d706
Luis Eduardo Perez Pacherrez <luis.perez@cdiproject.com>
2019-05-04 14:05:49 -0500

    feat(UUID): Extract the device UUID.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { UuidService } from "../services/uuid/uuid.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
-
+  uuid: any;
+  constructor(private uuidService: UuidService) {
+    this.uuid = this.uuidService.uuid;
+  }
 }
