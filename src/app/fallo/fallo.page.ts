2e2cf6e000def0a91a87c46fe08c1215f39fb9bf
Luis Eduardo Perez Pacherrez <luis.perez@cdiproject.com>
2019-05-06 18:43:27 -0500

    feat(Surveys): The labels of each survey are loaded.

--- src/app/fallo/fallo.page.ts
+++ src/app/fallo/fallo.page.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { OtsPendientesService } from '../services/ots/ots-pendientes/ots-pendientes.service';
+import { Component, OnInit } from "@angular/core";
+import { OtsPendientesService } from "../services/ots/ots-pendientes/ots-pendientes.service";
 
 @Component({
-  selector: 'app-fallo',
-  templateUrl: './fallo.page.html',
-  styleUrls: ['./fallo.page.scss'],
+  selector: "app-fallo",
+  templateUrl: "./fallo.page.html",
+  styleUrls: ["./fallo.page.scss"]
 })
 export class FalloPage implements OnInit {
   data: any;
   surveys: any;
   text: any;
-  constructor(private otsService: OtsPendientesService) { }
+  attributes_id: any;
+  widget_type: any;
+  result: string ="";
+  constructor(private otsService: OtsPendientesService) {}
 
   ngOnInit() {
     this.getSurveyFail();
@@ -25,5 +28,18 @@ export class FalloPage implements OnInit {
       console.log(this.surveys[0].domain_name);
       console.log(this.surveys);
     });
+  }
+
+  openSurvey(elemento) {
+    console.log("SURVEY", elemento);
+    for (let i = 0; i < elemento.groups.length; i++) {
+      for (let ii = 0; ii < elemento.groups[i].attributes.length; ii++) {
+      console.log("ATRIBUTOS_ID",elemento.groups[i].attributes[ii].attributes_id);
+      console.log("widget_type",elemento.groups[i].attributes[ii].widget_type);
+      let label = "";
+       label = `<label>${elemento.groups[i].attributes[ii].attributes_name}</label><br>`
+      this.result= this.result+ label;
+    }
+  }
   }
 }
