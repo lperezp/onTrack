c5a2960ed2c1d49613c319bd92cd34434f88a320
Luis Eduardo Perez Pacherrez <luis.perez@cdiproject.com>
2019-05-06 02:52:57 -0500

    fix(FalloPage): Load API text in the failed survey.

--- src/app/fallo/fallo.page.ts
+++ src/app/fallo/fallo.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OtsPendientesService } from '../services/ots/ots-pendientes/ots-pendientes.service';
 
 @Component({
   selector: 'app-fallo',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fallo.page.scss'],
 })
 export class FalloPage implements OnInit {
-
-  constructor() { }
+  data: any;
+  surveys: any;
+  text: any;
+  constructor(private otsService: OtsPendientesService) { }
 
   ngOnInit() {
+    this.getSurveyFail();
   }
 
+  getSurveyFail() {
+    this.otsService.getOTSPendientes().then(data => {
+      this.data = JSON.parse(data.data);
+      console.log("Resultado", this.data);
+      this.surveys = this.data["surveys"];
+      this.text = this.surveys[0].domain_name;
+      console.log(this.surveys[0].domain_name);
+      console.log(this.surveys);
+    });
+  }
 }
--- src/app/survey-fallo/survey-fallo.page.ts
+++ src/app/survey-fallo/survey-fallo.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OtsPendientesService } from '../services/ots/ots-pendientes/ots-pendientes.service';
 
 @Component({
   selector: 'app-survey-fallo',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./survey-fallo.page.scss'],
 })
 export class SurveyFalloPage implements OnInit {
+  data: any;
+  surveys: any;
+  attribute_values: any;
 
-  constructor() { }
+  constructor(private otsService: OtsPendientesService) { }
 
   ngOnInit() {
+    this.getSurveyFail();
   }
 
+  getSurveyFail() {
+    this.otsService.getOTSPendientes().then(data => {
+      this.data = JSON.parse(data.data);
+      console.log("Resultado", this.data);
+      this.surveys = this.data["surveys"];
+      this.attribute_values = this.data["attribute_values"];
+      console.log(this.surveys);
+      console.log(this.attribute_values);
+    });
+  }
 }
