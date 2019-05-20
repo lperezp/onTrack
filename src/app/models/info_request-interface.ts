426ee8110ec3a9e52a0855b221a75a8e34866fd6
Luis Eduardo Perez Pacherrez <luis.perez@cdiproject.com>
2019-05-20 13:31:46 -0500

    feat(InfoRequestInterface): The InfoRequestInterface interface was added.

---
+++ src/app/models/info_request-interface.ts
@@ -0,0 +1,9 @@
+export interface InfoRequestInterface {
+  service_statuses_id: number;
+  info_request_sequences_id: number;
+  info_request_group_sequence: number;
+  info_request_sequence: number;
+  domains_id: number;
+  domains_name: string;
+  info_request_updated_at: string;
+}
